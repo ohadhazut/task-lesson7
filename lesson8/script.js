5dee60800f283eb50a85c9399a933865a34a4ddf
ohadhazut <114682996+ohadhazut@users.noreply.github.com>
2022-11-07 17:45:46 +0200

    Add files via upload

---
+++ lesson8/script.js
@@ -0,0 +1,15 @@
+function gaga() {
+    let pizza = prompt('you want pizza with a chease?');
+    if (pizza == "yes") {
+        alert('pizza with extre chease');
+    }else if (pizza == "no")
+    alert ('pizza without extra chease');
+}
+
+function gigi() {
+    let hamburger = prompt('you want hamburger with tomato?');
+    if (hamburger == "yes") {
+        alert('hamburger with tomato');
+    }else if (hamburger == "no")
+    alert ('hamburger without tomato');
+}
